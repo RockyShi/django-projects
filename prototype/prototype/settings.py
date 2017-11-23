e30ff3e254d3027d2019617af5f134f16578913a
Rocky Shi <bigmortz@gmail.com>
2017-11-23 23:43:37 +0800

    - Make the website use MySQL server instead of local sqlite file.

--- prototype/prototype/settings.py
+++ prototype/prototype/settings.py
@@ -78,8 +78,17 @@ WSGI_APPLICATION = 'prototype.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'test',
+         'USER': 'dbtest',
+         'PASSWORD': 'dbtest66',
+         'HOST': '172.17.0.14',
+         'PORT': '3306',
+         'OPTIONS': {
+                 'sql_mode': 'STRICT_TRANS_TABLES',
+            },
     }
 }
 
