43f1e03865b70eed11f94d19f079fe96a2131edf
Rocky Shi <bigmortz@gmail.com>
2017-11-30 19:03:46 +0800

    Add framework 7 into project for UI.

--- prototype/wines/urls.py
+++ prototype/wines/urls.py
@@ -7,6 +7,10 @@ from . import views
 app_name = 'wines'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^main[/]?$', views.main, name='main'),
+    url(r'^about.html$', views.about, name='about'),
+    url(r'^services.html$', views.service, name='service'),
+    url(r'^add', views.add_wine, name='add_wine'),
     url(r'^wine/(?P<wine_id>[0-9]+)$', views.wine, name='wine'),
     url(r'^supplier/(?P<supplier_id>[0-9]+)/$', views.supplier, name='supplier'),
 ]
--- prototype/wines/views.py
+++ prototype/wines/views.py
@@ -8,6 +8,15 @@ from .models import Wine
 
 # Create your views here.
 
+def main(request):
+    return render(request, 'wines/main.html', {});
+
+def about(request):
+    return render(request, 'wines/about.html', {});
+
+def service(request):
+    return render(request, 'wines/services.html', {});
+
 def index(request):
     latest_wine_list = Wine.objects.order_by('-date')[:5]
     context = {
@@ -21,3 +30,11 @@ def wine(request, wine_id):
 
 def supplier(request, supplier_id):
     return HttpResponse("You're looking at Supplier %s" % supplier_id)
+
+def add_wine(request):
+    if request.method == 'GET':
+        return render(request, 'wines/add_wine.html', {})
+    else:
+        w = Wine(name = request.GET.get('name', None), wine_type = request.GET.get('wine_type', None), nation = request.GET.get('nation', None), district = request.GET.get('district', None), date = request.GET.get('date', None), shelf_life = request.GET.get('shelf_life', None), grape_type = request.GET.get('grape_type', None), alcohol = request.GET.get('alcohol', None), volume = request.GET.get('volume', None), supplier = request.GET.get('supplier', None))
+        w.save()
+
